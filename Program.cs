30e712ff8baa53df9d103c30694382b21a965c90
Salvador Ayala <salvador.angel.a.h@gmail.com>
2022-09-08 11:54:01 -0800

    Ejercicio menu principal

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
 ﻿IConsumer consumer = new StrategyClient();
 
 consumer.runClient();
+
+
+// TODO Implementar un menu para poder seleccionar cual ejercicio ejecutar
+// TODO Implementar un patron de diseño (Creacional)
