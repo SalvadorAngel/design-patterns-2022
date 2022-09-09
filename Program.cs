da80c4bfb2bced04759313ca7f054e979fbbe586
Salvador Ayala <salvador.angel.a.h@gmail.com>
2022-09-08 17:18:30 -0800

    Se agregaron notas de ejercicios nuevos investigacion

--- Program.cs
+++ Program.cs
@@ -5,3 +5,7 @@ consumer.runClient();
 
 // TODO Implementar un menu para poder seleccionar cual ejercicio ejecutar
 // TODO Implementar un patron de diseño (Creacional)
+
+
+// TODO Investigar y agregar ejemplo del patron de diseño Creacional "Prototype"
+// TODO Investigar y agregar ejemplo del patron de diseño Estructural "Flyweight"
